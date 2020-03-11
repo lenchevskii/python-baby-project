26d20e079e218dd2c2586e9a30efd32801979876
Dmitry Lenchevski <dima.lenchevskii@gmail.com>
2020-03-11 22:31:19 +0300

    add caching decorator

---
+++ source/caching-decorator.py
@@ -0,0 +1,12 @@
+def cache_last(func, *size):
+    cache = dict()
+
+    def memoized_func(*args):
+        if args in cache:
+            return cache[args]
+        result = func(*args)
+        cache[args] = result
+        return result
+
+    return memoized_func
+
