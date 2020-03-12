24f8d70fda007a799df7d43175f607b36efa7a6f
Dmitry Lenchevski <dima.lenchevskii@gmail.com>
2020-03-12 11:29:43 +0300

    refactoring

--- source/decipher/decipher.py
+++ source/decipher/decipher.py
@@ -2,21 +2,28 @@ import re
 import sys
 
 
+def is_message(txt):
+    return len(txt) < 100000 and re.findall(r"[^a-z]+", txt) == []
+
+
+def is_identical_letters(txt, index):
+    return txt[index] == txt[index - 1]
+
+
 def decipher(txt):
-    if len(txt) < 100000 and re.findall(r"[^a-z]+", txt) == []:
-        res = ''
+    if is_message(txt):
+        res = []
         i = 1
         while i < len(txt):
-            if txt[i] != txt[i - 1]:
-                res = ''.join([res, txt[i - 1]])
+            if not is_identical_letters(txt, i):
+                res.append(txt[i - 1])
                 if i == len(txt) - 1:
-                    res = ''.join(res, [txt[i]])
+                    res.append(txt[i])
                 i = i + 1
             else:
                 i = i + 2
-        return res
-    else:
-        return 'This is a trapppp!'
+        return ''.join(res)
+    return 'This is a trapppp!'
 
 
 input_txt = open(sys.argv[1])
