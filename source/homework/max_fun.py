0f6513d9999ffa4fd3ba8d4fb49384c38f61c839
dmitry <dima.lenchevskii@gmail.com>
2020-06-26 15:23:14 +0300

    fix max function

--- source/homework/max_fun.py
+++ source/homework/max_fun.py
@@ -1,8 +1,32 @@
-from functools import reduce
+from functools import cmp_to_key
+from itertools import permutations
 
 
-def max_from_list(li):
-    return ''.join(sorted(reduce(lambda x, y: str(x) + str(y), li), reverse=True))
+def stupid_max(input_list):
+    return max(map(lambda x: ''.join(map(lambda y: str(y), x)), permutations(input_list)))
 
 
-print(max_from_list([4, 2, 3]))
+def fine_max(input_list):
+    return sorted(input_list, key=cmp_to_key(compare))
+
+
+def compare(x, y):
+    if "{}{}".format(x, y) > "{}{}".format(y, x):
+        return -1
+    elif "{}{}".format(x, y) < "{}{}".format(y, x):
+        return 1
+    else:
+        return 0
+
+
+def max_fine_recursion(input_list):
+    if not input_list:
+        return []
+    first, *tail = input_list
+    return max_fine_recursion([a for a in tail if "{}{}".format(a, first) > "{}{}".format(first, a)]) + [
+        first] + max_fine_recursion([a for a in tail if "{}{}".format(a, first) < "{}{}".format(first, a)])
+
+
+print(max_fine_recursion([5299, 52]))
+print(max_fine_recursion([1234, 9]))
+print(max_fine_recursion([98, 9, 34]))
